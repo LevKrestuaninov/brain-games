0272b44090c24f367e9bdff8e3a89b286361089a
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-03-05 14:38:53 +0300

    fix eslin errors

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -7,39 +7,38 @@ const incorrectAnswer = "Let's try again, ";
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const genLength = () => {
-    let lineLength = 0;
-    while (lineLength < 5) {
-        lineLength = getRandomInt(10);
-    }
-    return lineLength;
+  let lineLength = 0;
+  while (lineLength < 5) {
+    lineLength = getRandomInt(10);
+  }
+  return lineLength;
 };
 const genArray = (array, lineLength) => {
-    let d = 0;
-    while (d < 1) {
-        d = getRandomInt(10);
-    } 
-    let a1 = getRandomInt(100)
-    for (let i = 0; i < lineLength; i += 1) {
-        array.push(a1);
-        a1 += d;
-    }
-    return array;
-}
+  let d = 0;
+  while (d < 1) {
+    d = getRandomInt(10);
+  }
+  let a1 = getRandomInt(100);
+  for (let i = 0; i < lineLength; i += 1) {
+    array.push(a1);
+    a1 += d;
+  }
+  return array;
+};
 
 const mainPart = () => {
   let array = [];
-  array =genArray(array, genLength());
-  let newArray = [... array];
-  let newArrayIndex = getRandomInt(newArray.length)
+  array = genArray(array, genLength());
+  const newArray = [...array];
+  const newArrayIndex = getRandomInt(newArray.length);
   newArray[newArrayIndex] = '...';
   console.log(`${'Question:'} ${newArray}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  if (parseInt(userAnswer) === array[newArrayIndex]) {
+  if (parseInt(userAnswer, 10) === array[newArrayIndex]) {
     return 1;
-  } else {
+  }
   console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${array[newArrayIndex]}'`);
   return 0;
-  }
 };
 
 indexFunction(firstStatement, mainPart, correctAnswer, incorrectAnswer);
