1ba3bbaa820fcabad22f8b1bb09f2c050eaf9f6e
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-03-05 18:53:58 +0300

    Revert "replace game files"
    This reverts commit 4e0fccc0de72db8f5e613ab0bb0efd718ddc56a2.

--- bin/games/brain-prime/brain-prime.js
+++ bin/games/brain-prime/brain-prime.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import indexFunction from '../../src/index.js';
+import indexFunction from '../../../src/index.js';
 
 const firstStatement = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const correctAnswer = 'Correct!';
