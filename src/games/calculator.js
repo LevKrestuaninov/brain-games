fabe2fa576b11c57e14d9cf0fec92528bf8e4f8c
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-03-18 18:22:14 +0300

    update calculator.js

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -3,34 +3,36 @@ import getRandomInt from '../utils.js';
 
 const gameRules = 'What is the result of the expression?';
 
-const doMathOperation = (operation, randomNumber1, randomNumber2) => {
+const doMathOperation = (operation, num1, num2) => {
   let res = 0;
   switch (operation) {
-    case 0:
-      res = randomNumber1 + randomNumber2;
+    case '+':
+      res = num1 + num2;
       return res;
-    case 1:
-      res = randomNumber1 - randomNumber2;
+    case '-':
+      res = num1 - num2;
       return res;
-    case 2:
-      res = randomNumber1 * randomNumber2;
+    case '*':
+      res = num1 * num2;
       return res;
     default:
-      break;
+      throw new Error(`Operator ${operator} - is incorrect!`)
   }
   return NaN;
 };
 
+const getRandomOperator = () => { 
+  const operators = ['+', '-', '*']; 
+  return operators [getRandomInt(0, operators.length - 1)] 
+}
+
 const getData = () => {
-  const operations = ['+', '-', '*'];
   const randomNumber1 = getRandomInt(0, 10);
   const randomNumber2 = getRandomInt(0, 10);
-  const operation = getRandomInt(0, 2);
-  const question = `${randomNumber1} ${operations[operation]} ${randomNumber2}`;
+  const operation = getRandomOperator();
+  const question = `${randomNumber1} ${operation} ${randomNumber2}`;
   const correctAnswer = `${doMathOperation(operation, randomNumber1, randomNumber2)}`;
   return [question, correctAnswer];
 };
 
-const calculator = () => runEngine(gameRules, getData);
-
-export default calculator;
+export default () => runEngine(gameRules, getData);
