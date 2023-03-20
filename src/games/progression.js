c5ca38ee0b7e4b7d93a42931a1387e62cbb0cc0a
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-03-20 19:28:38 +0300

    change gen progression in progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,25 +3,26 @@ import getRandomInt from '../utils.js';
 
 const gameRules = 'What number is missing in the progression?';
 
-const genArray = (array, lineLength) => {
-  const d = getRandomInt(1, 10);
-  let a1 = getRandomInt(0, 100);
-  for (let i = 0; i < lineLength; i += 1) {
-    array.push(a1);
-    a1 += d;
+const generateProgression  = (a1, d, length) => {
+  const progression = [];
+  let difference = a1;
+  for (let i = 0; i < length; i += 1) {
+    progression.push(difference);
+    difference += d;
   }
-  return array;
+  return progression;
 };
 
 const getDate = () => {
   const arrayLength = getRandomInt(5, 10);
-  let array = [];
-  array = genArray(array, arrayLength);
-  const newArray = [...array];
-  const newArrayIndex = getRandomInt(0, newArray.length - 1);
-  newArray[newArrayIndex] = '..';
-  const question = `${newArray.join(' ')}`;
-  const correctAnswer = `${array[newArrayIndex]}`;
+  const d = getRandomInt(1, 10);
+  const a1 = getRandomInt(0, 100);
+  const progression = generateProgression (a1, d, arrayLength);
+  const newProgression = [...progression];
+  const newProgressionIndex = getRandomInt(0, newProgression.length - 1);
+  newProgression[newProgressionIndex] = '..';
+  const question = `${newProgression.join(' ')}`;
+  const correctAnswer = `${progression[newProgressionIndex]}`;
   return [question, correctAnswer];
 };
 
